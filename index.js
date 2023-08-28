953a159ac2e6f7ecf3f7499956f0793203873490
JafetSena <jasena2002@hotmail.com>
2023-08-28 08:48:44 -0500

    NPM modulo

---
+++ index.js
@@ -0,0 +1,13 @@
+let frases=[
+    "frase 1",
+    "frase 2",
+    "frase 3"
+]
+
+function obtenerFrase() {
+    return frases(indice);
+}
+
+module.exports.obtenerFrase = obtenerFrase();
+
+console.log(modulo);
